0b9c20ad98fa7a15fc290f37e363e7c0ca1c853e
Kim Pepper <kim@previousnext.com.au>
2021-06-03 09:27:58 +1000

    [semver:patch] Fix patchy commit message

--- src/scripts/patchy.sh
+++ src/scripts/patchy.sh
@@ -15,15 +15,17 @@ composer2 update --prefer-dist --no-interaction --no-progress --no-suggest
 if git diff-files --quiet --ignore-submodules -- composer.lock ; then
   echo "No composer changes"
 else
-  touch /tmp/commit-message.txt
-  ./bin/composer-lock-diff --md --no-links >> /tmp/commit-message.txt
+  echo "[PATCHY] Updates composer dependencies" >> /tmp/commit-message.txt
+  echo "" >> /tmp/commit-message.txt
+  ./bin/composer-lock-diff --md --no-links >> /tmp/lock-diff.txt
+  cat /tmp/lock-diff.txt >> /tmp/commit-message.txt
   git add composer.lock
   git commit -F /tmp/commit-message.txt
   make config-import updb config-export
   git add config-export
   git commit -m "[PATCHY] Updates config" || echo "No config changes"
   git push -f origin $PATCHY_BRANCH
-  gh pr create -t "[PATCHY] Updates composer dependencies" -F /tmp/commit-message.txt || true
+  gh pr create -t "[PATCHY] Updates composer dependencies" -F /tmp/lock-diff.txt || true
 fi
 # Reset defaults.
 git checkout $CURRENT_BRANCH
